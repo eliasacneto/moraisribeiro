9bfe29836084c092f33cf88d6347c366ff39a559
rodrigmeira <meiramuisc@outlook.com>
2024-04-22 12:44:09 -0300

    Navbar + Hero section

---
+++ app/components/Home/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='pt-[5rem] pb-[3rem]'>
+        <div className="w-[100%] h-[60vh] flex flex-col items-center justify-center"></div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './Hero';
+
+const Home = () => {
+  return (
+    <div className="relative w-screen h-screen bg-hero bg-cover">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
