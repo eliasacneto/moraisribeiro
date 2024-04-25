847facf005cc4e13ea9052dbca524f81ee739ef7
Elias Neto <netoeac.dev@gmail.com>
2024-04-24 21:53:17 -0300

    logo and links from navbar adjusted

--- app/components/Home/Nav.tsx
+++ app/components/Home/Nav.tsx
@@ -8,42 +8,40 @@ const Nav = () => {
         <div className="fixed top-0 left-0 right-0 z-50">
             <div className="w-full h-[10vh] bg-slate-950  border-b border-[#B8967E] ">
                 <div className="w-[90%] md:w-[80%] h-[100%] mx-auto flex items-center justify-between">
-                    <div className="w-[150px] h-[150px] md:w-[250px] md:h-[250px]">
-                        <Link href="/">
-                            <Image
-                                src={Logo}
-                                alt="Logo"
-                                width={250}
-                                height={250}
-                                className="w-[100%] h-[100%] p-4"
-                            />
-                        </Link>
-                    </div>
+                    <Link href="/">
+                        <Image
+                            src={Logo}
+                            alt="Logo"
+                            width={250}
+                            height={100}
+                            className="w-[100%] h-[100%]"
+                        />
+                    </Link>
                     <ul className="flex space-x-16 text-white text-[17px] ml-96 ">
-                        <li>
-                            <Link href="/" className="hover:text-gray-300">
+                        <li className='text-zinc-300'>
+                            <Link href="/" className="hover:text-[#cca185] transition duration-500 hover:duration-500">
                                 Início
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/areas" className="hover:text-gray-300">
+                        <li className='text-zinc-300'>
+                            <Link href="/areas" className="hover:text-[#cca185] transition duration-500 hover:duration-500">
                                 Áreas de atuação
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/team" className="hover:text-gray-300">
+                        <li className='text-zinc-300'>
+                            <Link href="/team" className="hover:text-[#cca185] transition duration-500 hover:duration-500">
                                 Profisisonais
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/about-us" className="hover:text-gray-300">
+                        <li className='text-zinc-300'>
+                            <Link href="/about-us" className="hover:text-[#cca185] transition duration-500 hover:duration-500">
                                 Sobre nós
                             </Link>
                         </li>
-                        <li>
+                        <li className='text-zinc-300'>
                             <Link
                                 href="/contact"
-                                className="bg-[#CC926A] text-white py-3 px-10 rounded-tl-[15px] rounded-br-[15px] px-10 py-3 hover:bg-gray-700"
+                                className="bg-[#d68e5d] text-white py-3 px-10 rounded-tl-[15px] rounded-br-[15px] px-10 py-3 hover:bg-[#c47642] text-bold transition duration-500 hover:duration-500"
                             >
                                 Contato
                             </Link>
