6eab0c238f2d3ee51c9a985c6f15db3f8fbde409
Murillo Alves Couto <murilloacouto@outlook.com>
2024-04-29 18:23:27 -0300

    applying responsiveness and finishing about us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -7,6 +7,8 @@ import eyeIcon from '../../public/images/eyeIcon.svg'
 import trophyIcon from '../../public/images/trophyIcon.svg'
 import Image from 'next/image';
 import aboutUsPhoto from '../../public/images/aboutUsPhoto.svg'
+import Button from '../components/Button/button';
+import Footer from '../components/Footer/Footer';
 
 export const metadata: Metadata = {
     title: "Sobre nós - Morais & Ribeiro",
@@ -18,13 +20,13 @@ const AboutUs = () => {
         <div>
             <SectionTitle title='Sobre nós' subtitle='Uma história de sucesso e confiança' />
             <div className="flex justify-center">
-                <div className="mt-24 mb-32 px-10 grid grid-cols-3 gap-24">
+                <div className="mt-20 sm:my-24 mb-20 px-4 md:px-10 grid grid-cols-1 md:grid-cols-3 gap-8">
                     <AboutUsCard 
                         image={targetIcon} 
                         alt={"Ícone de alvo"}
                         title={"Missão"} 
                         text={"Nossa missão é baseada em três pilares: foco, conhecimento e inovação. Estamos dedicados a entregar soluções jurídicas e de negócios que superem as expectativas dos clientes e desafiem os paradigmas tradicionais da advocacia."}
-                        border="border-[#40B8967E]"
+                        border="border-[#b8967e42]"
                         rounded="rounded-tr-[30px] rounded-bl-[30px]"
                     />
                     <AboutUsCard 
@@ -40,28 +42,40 @@ const AboutUs = () => {
                         alt={"Ícone de alvo"}
                         title={"Valores"} 
                         text={"Transparência total, soluções eficazes, empatia e comprometimento. Somos comprometidos a seguir os princípios fundamentais do Direito, valorizando a confiança dos clientes e buscando construir relacionamentos duradouros baseados em transparência, ética e excelência jurídica."}
-                        border="border-[#40B8967E]"
+                        border="border-[#b8967e42]"
                         rounded="rounded-tr-[30px] rounded-bl-[30px]"
                     />
                 </div>
             </div>
-            <div className={"flex"}>
-                <div>
+            <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 mb-16 sm:mb-16">
+                <div className="md:w-1/2 mb-8 md:mb-0">
                     <Image 
-                    src={aboutUsPhoto} 
-                    alt={"Doutor João Pedro, Doutora Nikolly Morais e Doutor Gustavo Santos"}
-                    className={"w-auto h-auto ml-14 mb-36 mr-10"}  />
+                        src={aboutUsPhoto} 
+                        alt={"Doutor João Pedro, Doutora Nikolly Morais e Doutor Gustavo Santos"}
+                        className='md:ml-16'
+                    />
                 </div>
-                <div>
-                    <p>texto</p>
-                    <h3>quem somos</h3>
-                    <p>texto texto texto</p>
-                    <button>Fale conosco agora</button>
+                <div className="md:w-1/2 md:pr-24 md:pl-4">
+                    <p className="text-sm mb-4 text-[#C98264]">
+                        EXPERIÊNCIA E QUALIFICAÇÃO
+                    </p>
+                    <h2 className="text-3xl text-black font-bold mb-5">
+                        QUEM SOMOS?
+                    </h2>
+                    <div>
+                        <p className="text-[17px] text-[#8B94A1] text-justify mb-8">
+                            Morais & Ribeiro, onde a excelência e a experiência se encontram para defender seus direitos com precisão e dedicação. Fundado por três colegas de faculdade, nosso escritório floresceu a partir de uma amizade sólida formada nos corredores acadêmicos e consolidada por uma visão comum: fornecer serviços jurídicos de alta qualidade, personalizados para atender às necessidades específicas de cada cliente. 
+                            <br /> 
+                            <br />
+                            Nosso compromisso é proporcionar uma assistência jurídica eficiente e adaptável, garantindo que cada estratégia seja moldada para oferecer os melhores resultados possíveis. Em nosso escritório, você encontrará uma equipe altamente qualificada, pronta para enfrentar desafios e conduzir seus casos com a máxima integridade e competência.
+                        </p>
+                        <Button>Fale conosco agora</Button>
+                    </div>
                 </div>
             </div>
-            
+            <Footer />
         </div>
     )
 }
 
-export default AboutUs
+export default AboutUs;
