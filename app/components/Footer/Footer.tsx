5dc5f8f4d3f8db11804f3c0378d10d6bf5bfbc97
Murillo Alves Couto <murilloacouto@outlook.com>
2024-04-29 20:12:58 -0300

    adding scrollToTopButton at Footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,15 +3,16 @@ import Logo from "@/public/images/lawlogo.svg";
 import Image from 'next/image';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; import { fa0 } from '@fortawesome/free-solid-svg-icons/fa0';
-import { faInstagram, faSquareWhatsapp, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faMap } from '@fortawesome/free-regular-svg-icons';
+import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton';
 
 const Footer = () => {
     return (
 
         <div className='flex flex-col sm:flex-row w-full justify-around bg-slate-950 px-4 sm:px-16 py-12'>
             <Image src={Logo} alt='logo' className='w-4/5 mb-10 sm:w-64' />
-            <div className='flex flex-col gap-8 sm:flex-row sm:justify-around sm:gap-12 pr-4 sm:pr-8'>
+            <div className='flex flex-col gap-8 sm:flex-row sm:justify-around sm:gap-12 pr-4 '>
                 <div>
                     <h3 className='text-[#CC926A] text-lg sm:text-xl mb-4 font-bold'>Acesso rápido</h3>
                     <ul className='list-none antialiased w-54 sm:w-54'>
@@ -52,6 +53,7 @@ const Footer = () => {
                             </Link>
                         </li>
                     </ul>
+                    <ScrollToTopButton />
                 </div>
             </div>
         </div>
