b5f69cee70d311e3681bcbb1cf86f535002b0933
Elias Neto <netoeac.dev@gmail.com>
2024-05-02 19:27:16 -0300

    form added and connected with Discord hook

--- app/components/ContactForm/ContactForm.client.tsx
+++ app/components/ContactForm/ContactForm.client.tsx
@@ -1,94 +1,345 @@
-'use client';
+"use client";
 
-import React, { FormEvent } from 'react';
-import Swal from 'sweetalert2';
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const ContactForm = () => {
+  const initialState = {
+    client_name: "",
+    email: "",
+    whatsapp: "",
+    interest: "",
+    metUs: "",
+    message: "",
+  };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log('Form aqui');
-        Swal.fire({
+  const [form, setForm] = useState(initialState);
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      client_name: event.target.value,
+    });
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      email: event.target.value,
+    });
+  };
+
+  const handleWhatsAppChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      whatsapp: event.target.value,
+    });
+  };
+
+  const handleInterestChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      interest: event.target.value,
+    });
+  };
+
+  const handleMetUsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      metUs: event.target.value,
+    });
+  };
+
+  const handleMessageChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setForm({
+      ...form,
+      message: event.target.value,
+    });
+  };
+
+  const sendForm = () => {
+    const { client_name, email, whatsapp, interest, metUs, message } = form;
+
+    console.log(`Nome: ${client_name}`);
+    console.log(`Email: ${email}`);
+    console.log(`Mensagem: ${message}`);
+
+    var webhookUrl =
+      "https://discordapp.com/api/webhooks/1235704230703271976/t82KP2nVaMwLXwA2DP35wNrt4yBXCnBE7af27ZUm46b6eJKkmD0jjfTzMgRVlY9bp-UZ";
+
+    var data = {
+      username: "M&R Bot",
+      content: "Uma nova mensagem foi recebida através do formulário:",
+      embeds: [
+        {
+          title: "Detalhes do Contato",
+          fields: [
+            { name: "Nome:", value: client_name, inline: true },
+            { name: "Email:", value: email, inline: true },
+            { name: "WhatsApp:", value: whatsapp, inline: true },
+            { name: "Mensagem:", value: message },
+          ],
+          color: 7506394,
+        },
+      ],
+    };
+
+    fetch(webhookUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          Swal.fire({
             title: "Sua mensagem foi enviada!",
             icon: "success",
             width: 600,
             padding: "3em",
             color: "#FFF",
-            background: "#0F172A",
+            background: "#1E1732",
             showConfirmButton: false,
             timer: 4000,
             showClass: {
-                popup: `
+              popup: `
+          animate__animated
+          animate__fadeInUp
+          animate__faster
+        `,
+            },
+            hideClass: {
+              popup: `
+          animate__animated
+          animate__fadeOutDown
+          animate__faster
+        `,
+            },
+          });
+        } else {
+          Swal.fire({
+            title: "Poxa...",
+            text: "Parece que ocorreu um erro ao enviar mensagem!",
+            icon: "error",
+            width: 600,
+            padding: "3em",
+            color: "#FFF",
+            background: "#1E1732",
+            showConfirmButton: false,
+            timer: 4000,
+            showClass: {
+              popup: `
+              animate__animated
+              animate__fadeInUp
+              animate__faster
+            `,
+            },
+            hideClass: {
+              popup: `
+              animate__animated
+              animate__fadeOutDown
+              animate__faster
+            `,
+            },
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao enviar dados: ", error);
+        Swal.fire({
+          title: "Erro de comunicação",
+          text: "Mas calma, já estamos trabalhando para resolver! ",
+          icon: "error",
+          width: 600,
+          padding: "3em",
+          color: "#FFF",
+          background: "#1E1732",
+          showConfirmButton: false,
+          timer: 4000,
+          showClass: {
+            popup: `
+          animate__animated
+          animate__fadeInUp
+          animate__faster
+        `,
+          },
+          hideClass: {
+            popup: `
+          animate__animated
+          animate__fadeOutDown
+          animate__faster
+        `,
+          },
+        });
+      });
+  };
+
+  const handleSubmit = (
+    event: React.FormEvent<
+      | HTMLFormElement
+      | HTMLInputElement
+      | HTMLSelectElement
+      | HTMLTextAreaElement
+    >
+  ) => {
+    event.preventDefault();
+
+    console.log("Form aqui");
+    sendForm();
+    Swal.fire({
+      title: "Sua mensagem foi enviada!",
+      icon: "success",
+      width: 600,
+      padding: "3em",
+      color: "#FFF",
+      background: "#0F172A",
+      showConfirmButton: false,
+      timer: 4000,
+      showClass: {
+        popup: `
   animate__animated
   animate__fadeInUp
   animate__faster
-`
-            },
-            hideClass: {
-                popup: `
+`,
+      },
+      hideClass: {
+        popup: `
   animate__animated
   animate__fadeOutDown
   animate__faster
-`
-            }
-        });
-    }
-    return (
-        <form className='flex flex-col w-[640px]' onSubmit={handleSubmit}>
-            <label>Nome completo<span className='text-rose-500'>*</span></label>
-            <input name='nome' className='border p-2.5 rounded-lg' type='text' placeholder='Ex.: João da Silva' />
+`,
+      },
+    });
+  };
 
-            <div className='flex flex-row justify-between gap-4 mt-3'>
-                <div className='flex flex-col rounded w-full'>
-                    <label>Email<span className='text-rose-500'>*</span></label>
-                    <input name='email' className='border p-2.5 rounded-lg w-full' type='email' placeholder='email@email.com' />
-                </div>
-                <div className='flex flex-col rounded w-full'>
-                    <label>WhatsApp<span className='text-rose-500'>*</span></label>
-                    <input name='whatsapp' className='border p-2.5 rounded-lg' type='text' placeholder='(00) 0 0000-0000' />
-                </div>
-            </div>
+  const interests = [
+    { value: "Direito Civil Geral", label: "Direito Civil Geral" },
+    { value: "Direito de Família", label: "Direito de Família" },
+    { value: "Direito Tributário", label: "Direito Tributário" },
+    { value: "Direito Empresarial", label: "Direito Empresarial" },
+    { value: "Direito do Trabalho", label: "Direito do Trabalho" },
+    { value: "Direito do Consumidor", label: "Direito do Consumidor" },
+    { value: "Direito Criminal", label: "Direito Criminal" },
+    { value: "Direito Previdenciário", label: "Direito Previdenciário" },
+    {
+      value: "Sou parte adversa em um processo",
+      label:
+        "Sou parte adversa em um processo (Informe abaixo o número do processo)",
+    },
+    {
+      value: "Quero fazer proposta de acordo",
+      label: "Quero fazer proposta de acordo",
+    },
+    {
+      value: "Outro assunto não especificado anteriormente",
+      label: "Outro assunto não especificado anteriormente",
+    },
+  ];
 
-            <div className='flex flex-row justify-between gap-4 mt-3'>
-                <div className='flex flex-col rounded w-full'>
-                    <label>Área de interesse<span className='text-rose-500'>*</span></label>
-                    <select name="area" className="bg-white border text-gray-900 rounded-lg block w-full p-2.5">
-                        <option value="" selected>Escolha uma área</option>
-                        <option value="Direito Civil Geral">Direito Civil Geral</option>
-                        <option value="Direito de Família">Direito de Família</option>
-                        <option value="Direito Tributário">Direito Tributário </option>
-                        <option value="Direito Empresarial">Direito Empresarial</option>
-                        <option value="Direito do Trabalho">Direito do Trabalho</option>
-                        <option value="Direito do Consumidor">Direito do Consumidor</option>
-                        <option value="Direito Criminal">Direito Criminal</option>
-                        <option value="Direito Previdenciário">Direito Previdenciário</option>
-                        <option value="Sou parte adversa em um processo">Sou parte adversa em um processo (Informe abaixo o número do processo) </option>
-                        <option value="Quero fazer proposta de acordo">Quero fazer proposta de acordo</option>
-                        <option value="Outro assunto não especificado anteriormente">Outro assunto não especificado anteriormente</option>
-                    </select>
-                </div>
-                <div className='flex flex-col rounded w-full'>
-                    <label>Como nos conheceu?</label>
-                    <select name="conhecimento" className="bg-white border text-gray-900 rounded-lg block w-full p-2.5">
-                        <option value="" selected>Selecione uma opção</option>
-                        <option value="area01">Indicação de um amigo</option>
-                        <option value="area02">Google</option>
-                        <option value="area03">Instagram</option>
-                        <option value="area04">Linkedin</option>
-                    </select>
-                </div>
-            </div>
+  return (
+    <form className="flex flex-col w-[640px]" onSubmit={handleSubmit}>
+      <label>
+        Nome completo<span className="text-rose-500">*</span>
+      </label>
+      <input
+        name="nome"
+        className="border p-2.5 rounded-lg"
+        type="text"
+        placeholder="Ex.: João da Silva"
+        value={form.client_name}
+        onChange={handleNameChange}
+        required
+      />
 
-            <label className='mt-5'>Mensagem<span className='text-rose-500'>*</span></label>
-            <textarea name='mensagem' className="block p-2.5 w-full text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Escreva aqui..." rows={6}></textarea>
+      <div className="flex flex-row justify-between gap-4 mt-3">
+        <div className="flex flex-col rounded w-full">
+          <label>
+            Email<span className="text-rose-500">*</span>
+          </label>
+          <input
+            name="email"
+            className="border p-2.5 rounded-lg w-full"
+            type="email"
+            placeholder="email@email.com"
+            value={form.email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="flex flex-col rounded w-full">
+          <label>
+            WhatsApp<span className="text-rose-500">*</span>
+          </label>
+          <input
+            name="whatsapp"
+            className="border p-2.5 rounded-lg"
+            type="text"
+            placeholder="(00) 0 0000-0000"
+            value={form.whatsapp}
+            onChange={handleWhatsAppChange}
+            required
+          />
+        </div>
+      </div>
 
-            <div className='flex justify-end mt-8'>
-                <button className="bg-[#d68e5d] text-white font-bold py-3 px-10 rounded-tl-[15px] rounded-br-[15px] px-10 py-3 hover:bg-[#c47642] text-bold transition duration-500 hover:duration-500" type='submit'>
-                    Enviar mensagem
-                </button>
-            </div>
-        </form >
-    );
+      <div className="flex flex-row justify-between gap-4 mt-3">
+        <div className="flex flex-col rounded w-full">
+          <label>
+            Área de interesse<span className="text-rose-500">*</span>
+          </label>
+          <select
+            name="area"
+            className="bg-white border text-gray-900 rounded-lg block w-full p-2.5"
+          >
+            <option value="">Escolha uma área</option>
+            {interests.map((interest, index) => (
+              <option key={index} value={interest.value}>
+                {interest.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-col rounded w-full">
+          <label>Como nos conheceu?</label>
+          <select
+            name="conhecimento"
+            className="bg-white border text-gray-900 rounded-lg block w-full p-2.5"
+          >
+            <option value="" selected>
+              Selecione uma opção
+            </option>
+            <option value="area01">Indicação de um amigo</option>
+            <option value="area02">Google</option>
+            <option value="area03">Instagram</option>
+            <option value="area04">Linkedin</option>
+          </select>
+        </div>
+      </div>
+
+      <label className="mt-5">
+        Mensagem<span className="text-rose-500">*</span>
+      </label>
+      <textarea
+        name="mensagem"
+        className="block p-2.5 w-full text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        placeholder="Escreva aqui..."
+        rows={6}
+        value={form.message}
+        onChange={handleMessageChange}
+        required
+      />
+
+      <div className="flex justify-end mt-8">
+        <button
+          className="bg-[#d68e5d] text-white font-bold py-3 px-10 rounded-tl-[15px] rounded-br-[15px] px-10 py-3 hover:bg-[#c47642] text-bold transition duration-500 hover:duration-500"
+          type="submit"
+        >
+          Enviar mensagem
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,58 +1,63 @@
-import { Metadata } from 'next';
-import React from 'react'
-import Footer from '../components/Footer/Footer';
-import { Marcellus } from 'next/font/google';
-import SectionTitle from '../components/SectionTitle/SectionTitle';
+import { Metadata } from "next";
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import { Marcellus } from "next/font/google";
+import SectionTitle from "../components/SectionTitle/SectionTitle";
 
-import CardIcon from '../components/CardIcon/CardIcon';
-import PinLocation from '../../public/images/location.svg'
-import WhatsApp from '../../public/images/whatsapp.svg'
-import ContactForm from '../components/ContactForm/ContactForm.client';
+import CardIcon from "../components/CardIcon/CardIcon";
+import PinLocation from "../../public/images/location.svg";
+import WhatsApp from "../../public/images/whatsapp.svg";
+import ContactForm from "../components/ContactForm/ContactForm.client";
 
 const font = Marcellus({
-    weight: ['400'],
-    subsets: ['latin']
-})
-
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-    title: "Entre em contato - Morais & Ribeiro",
-    description: "Advocacia e Consultoria Jurídica",
+  title: "Entre em contato - Morais & Ribeiro",
+  description: "Advocacia e Consultoria Jurídica",
 };
 
 const Contact = () => {
+  return (
+    <>
+      <SectionTitle
+        title="Entre em contato"
+        subtitle="Preencha o formulário abaixo"
+      />
 
-    return (
-        <>
-            <SectionTitle title='Entre em contato' subtitle='Preencha o formulário abaixo' />
+      <div className="flex flex-row justify-evenly items-center  my-8">
+        <div className="flex w-2/5 gap-10">
+          <CardIcon
+            title="Localização"
+            text="Rua Antônio Aleixo da Silva, 1880  Jardim Satélite, São José dos Campos - SP CEP 12231-650"
+            image={PinLocation}
+            alt="Pin Location"
+            border="border-[#b8967e42]"
+            rounded="rounded-tr-[3rem] rounded-bl-[3rem]"
+          />
+          <CardIcon
+            title="WhatsApp e E-mail"
+            text="(12) 9 9160-4267 moraisribeiroadvjur@gmail.com"
+            image={WhatsApp}
+            alt="Pin Location"
+            border="border-[#b8967e42]"
+            rounded="rounded-tr-[3rem] rounded-bl-[3rem]"
+          />
+        </div>
 
-            <div className='flex flex-row justify-evenly items-center  my-8'>
-                <div className='flex w-2/5 gap-10'>
-                    <CardIcon
-                        title='Localização'
-                        text='Rua Antônio Aleixo da Silva, 1880  Jardim Satélite, São José dos Campos - SP CEP 12231-650'
-                        image={PinLocation} alt='Pin Location' border="border-[#b8967e42]"
-                        rounded="rounded-tr-[3rem] rounded-bl-[3rem]"
+        <div className="bg-gray-50 p-10  rounded-3xl">
+          <h1 className="text-3xl">Entre em contato</h1>
+          <p className=" mt-3 mb-9">
+            <span className="text-rose-500">*</span>Campos obrigatórios
+          </p>
+          <ContactForm />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-                    />
-                    <CardIcon
-                        title='WhatsApp e E-mail'
-                        text='(12) 9 9160-4267 moraisribeiroadvjur@gmail.com'
-                        image={WhatsApp} alt='Pin Location' border="border-[#b8967e42]"
-                        rounded="rounded-tr-[3rem] rounded-bl-[3rem]"
-                    />
-
-                </div>
-
-                <div className='bg-gray-50 p-10  rounded-3xl' >
-                    <h1 className='text-3xl'>Entre em contato</h1>
-                    <p className=' mt-3 mb-9'><span className='text-rose-500'>*</span>Campos obrigatórios</p>
-                    <ContactForm />
-                </div >
-            </div >
-            <Footer />
-        </>
-    )
-}
-
-export default Contact
+export default Contact;
